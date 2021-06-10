015bb709f47cecce082894011f9c655e3bf7c584
iqballiii <meet.iqbal11@gmail.com>
2021-06-10 17:16:50 +0530

    added a package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() {
   runApp(MaterialApp(home: IAmRich()));
@@ -8,8 +10,9 @@ class IAmRich extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: Colors.blueGrey.shade400,
       appBar: AppBar(
+        backgroundColor: Colors.blueGrey.shade600,
         title: Text(
           'I am Rich!',
           style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
@@ -18,19 +21,31 @@ class IAmRich extends StatelessWidget {
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          SizedBox(
-            width: 180.0,
-            child: Image.asset('assets/diamond.png'),
+          Text(
+            'This is a picture \nof a diamond!',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+          ),
+          Center(
+            child: Image.asset(
+              'assets/diamond.png',
+              width: MediaQuery.of(context).size.width * 0.7,
+            ),
           ),
           SizedBox(
             height: 35.0,
           ),
-          Center(
-            child: Text(
-              'This is a picture of a diamond!',
-              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
-            ),
+          AnimatedTextKit(
+            animatedTexts: [
+              TyperAnimatedText('Shine bright like a DIAMOND!'),
+              TyperAnimatedText('Shine bright like a diamond'),
+              TyperAnimatedText('Find light in the beautiful sea'),
+              TyperAnimatedText('I choose to be happy'),
+              TyperAnimatedText('You and I, you and I'),
+            ],
+            repeatForever: true,
           ),
         ],
       ),
